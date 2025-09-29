805d1bb28b0a72af1d3fc1fb112fe7c9f95e6139
Ben Lawson <benlawsky99@gmail.com>
2025-09-29 14:03:05 +0100

    Copied in from the IDLE example

---
+++ first_demo.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+# Author: BLawson
+# Description: This script will demo
+"""
+    Docstring:
+"""
+#! /usr/bin/env python3
+#Author: Ben
+#Version: 1.0
+#This is our first script
+"""
+This is a multi line comment
+(doc string)
+"""
+
+# name = 'Ben Lawson'
+name = input("Ben Lawson: ")
+
+print("my name is", name, sep=" ")
+print("my name is " + name)
+
+import math
+print("cosine of 0.5 =", math.cos(0.5))
+
+import random
+print("My lucky number is", random.randint(1, 50))
+
+print("Done")
