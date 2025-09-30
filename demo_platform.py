a08cc15870c5425c55858648fd11f284b2ace782
Ben Lawson <benlawsky99@gmail.com>
2025-09-30 10:29:20 +0100

    Demo for testing platform and script (irrelevant)

--- demo_platform.py
+++ demo_platform.py
@@ -17,3 +17,18 @@ elif sys.platform == 'linux':
     print("Running on Linux")
 else:
     print("Other platform")
+
+
+import sys
+import os
+
+my_home = None
+
+if sys.platform == "win32":
+    my_home = os.environ['HOMEPATH']
+elif sys.platform == "darwin" or sys.platform == "linux":
+    my_home = os.environ['HOME']
+else:
+    print("On some other OS")
+
+print("Home is", my_home)
