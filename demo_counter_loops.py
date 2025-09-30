dd47c73ac54b595c343e4e48a23998489b4fdec1
Ben Lawson <benlawsky99@gmail.com>
2025-09-30 11:03:57 +0100

    demo counter to print numbers

---
+++ demo_counter_loops.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+# Author: BLawson
+# Description: This script will demo how to repeat a block of commands a specific
+# number of times using a counter loop.
+"""
+    Docstring:
+"""
+
+count = 0 # 1. Initialise counter
+while count < 10: # 2.Test condition
+    print(count)
+    count += 1 # 2.Increment/Decrement
+
