ce434eb2b61c0c88c7f5006a8a972472533343cd
Ben Lawson <benlawsky99@gmail.com>
2025-10-01 14:41:06 +0100

    Python project structure

---
+++ calculator/__init__.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+# Author: BLawson
+# Description: This script will demo
+"""
+    Docstring:
+"""
+
---
+++ calculator/app/__init__.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python3
+# Author: BLawson
+# Description: This script will demo
+"""
+    Docstring:
+"""
